062782199e9d586a303941e48bd5429d23ef42fa
Caramiozz <56002327+Caramiozz@users.noreply.github.com>
2022-01-06 22:11:26 +0300

    Update index.php

--- index.php
+++ index.php
@@ -8,31 +8,31 @@
   
   <body>
     <p>Hello Teacher itsa me!</p>
+     <p>Wazzup</p>
   </body> 
 
 <?php
 
- $serverName = "server-1200506101.database.windows.net"; // update me
+  $serverName = "your_server.database.windows.net"; // update me
     $connectionOptions = array(
-        "Database" => "mySampleDatabase", // update me
-        "Uid" => "SqlAdmin", // update me
-        "PWD" => "berk!@-95AlTek@1-" // update me
+        "Database" => "your_database", // update me
+        "Uid" => "your_username", // update me
+        "PWD" => "your_password" // update me
     );
     //Establishes the connection
     $conn = sqlsrv_connect($serverName, $connectionOptions);
-    $tsql= "SELECT * FROM tblEmployee WHERE EmpName = 'berk' ";
+    $tsql= "SELECT TOP 20 pc.Name as CategoryName, p.name as ProductName
+         FROM [SalesLT].[ProductCategory] pc
+         JOIN [SalesLT].[Product] p
+         ON pc.productcategoryid = p.productcategoryid";
     $getResults= sqlsrv_query($conn, $tsql);
     echo ("Reading data from table" . PHP_EOL);
-    if ($getResults == FALSE){
-        
-        echo "getResults is false";
-        echo $getResults;
-      
+    if ($getResults == FALSE)
+        echo (sqlsrv_errors());
+    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+     echo ($row['CategoryName'] . " " . $row['ProductName'] . PHP_EOL);
     }
-    
-    echo "echoing the final part";
-
-    sqlsrv_free_stmt($getResults[0]);
+    sqlsrv_free_stmt($getResults);
 
 ?>
   
